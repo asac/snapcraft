dc37fecf56d776fd9cb8eeff47c0744d3f9b3ef3
Alexander Sack <asac@ubuntu.com>
2016-01-26 18:18:49 +0100

    WIP global CLI

--- snapcraft/commands/build.py
+++ snapcraft/commands/build.py
@@ -23,6 +23,8 @@ Usage:
   build [options] [PART ...]
 
 Options:
+  -j [<threads>]        multi threaded build for plugins that support it; use 0 for cpu-count+1 [default: 0]
+  -V                    verbose built for plugins that support it.
   -h --help             show this help message and exit.
 
 """
@@ -30,10 +32,13 @@ Options:
 from docopt import docopt
 
 from snapcraft import lifecycle
-
+from snapcraft import common
 
 def main(argv=None):
     argv = argv if argv else []
     args = docopt(__doc__, argv=argv)
+
+    common.set_build_threads(args['-j'])
+    common.set_verbose(args['-V'])
 
     lifecycle.execute('build', args['PART'])
